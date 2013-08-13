c9c4237d0caf7f973d0bcab6c1385969cbd2a64e
hetfield <hetfield@mail15.com>
2013-08-13 18:29:00 +0400

    create mailertestcontroller

--- protected/components/Phpmailer.php
+++ protected/components/Phpmailer.php
@@ -996,7 +996,7 @@ class PHPMailer {
    * @return bool
    */
   protected function SmtpSend($header, $body) {
-    require_once $this->PluginDir . 'class.smtp.php';
+    require_once $this->PluginDir . 'Smtp.php';
     $bad_rcpt = array();
 
     if(!$this->SmtpConnect()) {
--- protected/controllers/TaskController.php
+++ protected/controllers/TaskController.php
@@ -38,6 +38,36 @@ class TaskController extends Controller
                     }
                 }
 
+                /** @var PHPMailer $phpMailer */
+                $phpMailer = new PHPMailer();
+
+                $phpMailer->CharSet="utf8";
+                $phpMailer->IsSMTP();
+                $phpMailer->Port       = 587;
+                $phpMailer->SMTPSecure = true;
+                $phpMailer->Host = 'smtp.mfxbroker.com';//'smtp.mfxbroker.com';
+                $phpMailer->Username = 'translations@mfxbroker.com';                            // SMTP username
+                $phpMailer->Password = 'aiutsraghuy545#';                           // SMTP password
+                $phpMailer->SMTPSecure = 'tls';
+                $phpMailer->SMTPAuth   = true;
+
+                $phpMailer->From = 'translations@mfxbroker.com';
+                $phpMailer->FromName = 'MasterForex';
+                $phpMailer->AddAddress('d.sukhov@mfxbroker.com', 'D.Sukhov');
+                $phpMailer->AddAddress('translations@mfxbroker.com', 'translations@mfxbroker.com');// Add a recipient
+
+                $phpMailer->IsHTML(true);                                  // Set email format to HTML
+
+                $phpMailer->Subject = 'Тема письма';
+                $phpMailer->Body    = 'This is the HTML message body <b>in bold!</b>';
+                $phpMailer->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+                if(!$phpMailer->Send()) {
+                    echo 'Message could not be sent.';
+                    echo 'Mailer Error: ' . $phpMailer->ErrorInfo;
+                    exit;
+                }
+
                 try{
                     if (!$recordText->save()){
                         throw new CException('Error during model saving.');
