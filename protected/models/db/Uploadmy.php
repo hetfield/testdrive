92f86c89946c8d5f4d2292cfbc6b7779b1b6ae6f
hetfield <hetfield@mail15.com>
2013-08-16 11:59:20 +0400

    including upload to task

---
+++ protected/models/db/Uploadmy.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: d.sukhov
+ * Date: 24.07.13
+ * Time: 12:24
+ * To change this template use File | Settings | File Templates.
+ *
+ * @property integer $ID
+ * @property string $Document
+ * @property string $TextId
+ */
+
+class Uploadmy extends CActiveRecord
+{
+    public $Document;
+
+    public static function model($className=__CLASS__)
+    {
+        return parent::model($className);
+
+    }
+
+    public function rules()
+    {
+        return array(
+            array('Document', 'file', 'types'=>'doc, docx'),
+        );
+    }
+}
