a2f78c9b8807e36207ff03370103251d170fdac8
hetfield <hetfield@mail15.com>
2013-09-27 16:52:29 +0400

    add another new phrase script

--- protected/controllers/PhrasesController.php
+++ protected/controllers/PhrasesController.php
@@ -75,3 +75,61 @@
 //fclose($fp);
 //die;
 //
+
+class PhrasesController extends Controller
+{
+    public function actionIndex()
+    {
+
+        set_time_limit(0);
+
+//алгоритм создания массива с данными таблицы для выгрузки в файлы
+        $phrases = Translations::model()->findAll();
+        /** @var Translations $phrase*/
+        $phraseAll = array();
+        foreach ($phrases as $phrase){
+            $phraseAll['ar'][$phrase->Category][$phrase->Key] = $phrase->LangAr;
+            $phraseAll['az'][$phrase->Category][$phrase->Key] = $phrase->LangAz;
+            $phraseAll['en'][$phrase->Category][$phrase->Key] = $phrase->LangEn;
+            $phraseAll['es'][$phrase->Category][$phrase->Key] = $phrase->LangEs;
+            $phraseAll['zh_cn'][$phrase->Category][$phrase->Key] = $phrase->LangCn;
+            $phraseAll['my'][$phrase->Category][$phrase->Key] = $phrase->LangMy;
+            $phraseAll['id'][$phrase->Category][$phrase->Key] = $phrase->LangId;
+            $phraseAll['ru'][$phrase->Category][$phrase->Key] = $phrase->LangRu;
+        }
+
+        json_encode($phraseAll);
+        exit();
+
+////выгрузка массива в файлы
+//        $path = Yii::app()->basePath.DIRECTORY_SEPARATOR.'messages-test'.DIRECTORY_SEPARATOR;
+//
+//        if(!is_dir($path)){
+//            mkdir($path);
+//        }
+//
+//        $arrayForFile = array();
+//
+//        foreach ($phraseAll as $lang => $categories) {
+//            foreach($categories as $category => $keys){
+//                foreach($keys as $key => $phrase){
+//                    $data = '"'.$key.'" => "'.$phrase.'"';
+//                    $arrayForFile[$lang][$category][] =  $data;
+//                }
+//            }
+//        }
+//        foreach ($arrayForFile as $lang => $categories){
+//            foreach($categories as $category => $phrase){
+//                if (!is_dir($path.$lang.DIRECTORY_SEPARATOR)){
+//                    mkdir($path.$lang.DIRECTORY_SEPARATOR);
+//                }
+//                $file = $path.$lang.DIRECTORY_SEPARATOR.$category.".php";
+//                $data = "<?php\nreturn array(\n    ".implode(",\n  ",$phrase)."\n);";
+//                file_put_contents($file, $data);
+//            }
+//        }
+//
+//        echo "done!\n";
+//        die;
+    }
+}
